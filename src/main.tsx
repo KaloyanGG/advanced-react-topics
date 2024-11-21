34aa508855a46b82bb22b981b0f6b3d9ac617cf3
Kaloyan Georgiev <81187395+KaloyanGG@users.noreply.github.com>
2024-11-21 16:07:02 +0200

    [refactor] Change loader

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./reset.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <App />
+  // </StrictMode>
 );

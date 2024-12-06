2d1ed3e9f56a23ed48ef4445036ea44999a758f4
Kaloyan Georgiev <81187395+KaloyanGG@users.noreply.github.com>
2024-12-06 15:32:54 +0200

    [feat] Static size carousel working (Likes functionality no)

--- src/components/recipeDetailsCard/RecipeDetailsCard.tsx
+++ src/components/recipeDetailsCard/RecipeDetailsCard.tsx
@@ -15,7 +15,7 @@ const RecipeDetailsCard = forwardRef<HTMLDivElement, RecipeDetailsCardProps>(
       <div
         onClick={onClick}
         ref={ref}
-        className={`recipe-details${!focused ? " outside" : ""}`}
+        className={`recipe-details${focused ? "" : " outside"}`}
         id={_id}
       >
         <div className='img'>
@@ -23,15 +23,20 @@ const RecipeDetailsCard = forwardRef<HTMLDivElement, RecipeDetailsCardProps>(
         </div>
         <div className='info-container'>
           <h1>Recipe: {name}</h1>
-          <h3>Ingredients:</h3>
+          <h2>Ingredients:</h2>
           <ul className='ingredients'>
             {ingredients.map((i) => {
               return <li key={i._id}>{i.name}</li>;
             })}
           </ul>
-          <h3>Instructions:</h3>
+          <h2>Instructions:</h2>
           <p>{instructions}</p>
-          <button className='like'>Likes: {likes}</button>
+          <button className='like'>
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+              <path d='M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853Z'></path>
+            </svg>
+            {likes}
+          </button>
         </div>
       </div>
     );

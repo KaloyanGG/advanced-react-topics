4dc8d60a5007232bccb170bebbaee0af2df4214d
Kaloyan Georgiev <81187395+KaloyanGG@users.noreply.github.com>
2024-12-12 15:39:20 +0200

    [refactor] Rename

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./reset.css";
-import Notificator from "./components/notificator/Notificator.tsx";
+import Notifications from "./components/notifications/Notifications.tsx";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
   <>
     <App />
-    <Notificator time={3000} />
+    <Notifications time={3000} />
   </>
   // </StrictMode>
 );

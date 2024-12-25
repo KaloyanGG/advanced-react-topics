861d14a806e10d798ea49a8a8a17ca223d3c4e26
Kaloyan Georgiev <81187395+KaloyanGG@users.noreply.github.com>
2024-12-25 20:32:45 +0200

    [feat] Number of favorite recipes

--- src/pages/homeLayout/HomeLayout.tsx
+++ src/pages/homeLayout/HomeLayout.tsx
@@ -4,16 +4,23 @@ import "./HomeLayout.css";
 import VwIndicator from "../../components/VWIndicator/VWIndicator";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useAppSelector } from "../../hooks";
 
 const queryClient = new QueryClient();
 const HomeLayout = () => {
+  const { ids } = useAppSelector((state) => state.likedRecipes);
   return (
     <>
       <VwIndicator />
       <nav>
         <NavLink to={"/"}>Recipes</NavLink>
         <NavLink to={"/addRecipe"}>Add recipe</NavLink>
-        <NavLink to={"/idk"}>idk</NavLink>
+        <NavLink to={"/favorites"}>
+          <span className='anchor-favorites'>
+            Favorites
+            <span className='badge'>{ids.length}</span>
+          </span>
+        </NavLink>
       </nav>
       <main>
         <QueryClientProvider client={queryClient}>

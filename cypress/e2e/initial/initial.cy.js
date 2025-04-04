e9374637e479f4c29ded458a99e0e2ff57bb70b7
Kaloyan Georgiev <81187395+KaloyanGG@users.noreply.github.com>
2025-04-04 11:06:15 +0300

    [text] Logging in, error, favoriting, like animation, 'hi' message, carousel, phone's loading text for ingredients

--- cypress/e2e/initial/initial.cy.js
+++ cypress/e2e/initial/initial.cy.js
@@ -94,4 +94,88 @@ describe("Recipes website", () => {
     // Navigate to login page
     cy.url().should("include", "/login");
   });
+
+  it("Tries to log in, sees error invalid credentials, logs in, sees notification, sees recipes, sees 'hi' message, user is in local storage", () => {
+    cy.visit("http://localhost:5173/login");
+    // Fill in form and submit
+    cy.get("input[name='email']").type("testuser");
+    cy.get("input[name='password']").type("testpassword");
+    cy.get("button[type='submit']").click();
+    // Show error for invalid credentials
+    cy.get("p.error").contains("Invalid credentials");
+    // Intercept next request and force it to resolve
+    cy.intercept(
+      {
+        method: "POST",
+        url: "http://192.168.1.12:3000/auth/login",
+      },
+      {
+        statusCode: 200,
+        body: {
+          email: "testuser@email.com",
+        },
+      }
+    );
+    // Submit the form again
+    cy.get("button[type='submit']").click();
+    // Show notification for successful login
+    cy.get("span.notification").contains("Login successful");
+    // Show recipes
+    cy.get("h2").contains("Spaghetti Carbonara");
+    // Sees text Hi testuser
+    cy.get("p.current-user").contains("Hi, testuser");
+    // Check if the user object exists in localStorage
+    cy.window().then((window) => {
+      const user = window.localStorage.getItem("user");
+      expect(user).to.not.be.null; // Ensure user is not null
+    });
+  });
+  describe.only("Logged in activities", () => {
+    // Login before each test
+    beforeEach(() => {
+      cy.visit("http://localhost:5173/login");
+      // Intercept the login request and force it to resolve
+      cy.intercept(
+        {
+          method: "POST",
+          url: "http://192.168.1.12:3000/auth/login",
+        },
+        {
+          statusCode: 200,
+          body: {
+            email: "testuser@email.com",
+          },
+        }
+      );
+      // Fill in form and submit
+      cy.get("input[name='email']").type("testuser");
+      cy.get("input[name='password']").type("testpassword");
+      cy.get("button[type='submit']").click();
+    });
+    it("Goes to recipes, likes a recipe, sees animation, favourite's number is 1, goes to the next one's recipe details, scrolls to the previous, previous is liked", () => {
+      cy.visit("http://localhost:5173");
+      // Click like button
+      cy.get("button.like").first().click();
+      // Click on the second "Read recipe" button
+      cy.get("button.read-recipe-button").eq(1).click();
+      // Sees animation
+      cy.get("button.like").first().should("have.class", "animate");
+      // Sees 1 favorite in the nav
+      cy.get("span.badge").contains("1");
+      // Click on the previous recipe
+      cy.wait(1000);
+      cy.get("div.outside").eq(1).click();
+      // Check if the 3rd recipe is liked
+      cy.get("button.like").eq(2).find("svg").should("have.class", "liked");
+      // Make the window to phone size
+      cy.viewport("iphone-x");
+      // Refresh page
+      cy.reload();
+      // Check if the <strong> contains "Loading"
+      cy.get("strong").contains("Loading");
+    });
+    it.only("", () => {
+      cy.visit("http://localhost:5173");
+    });
+  });
 });
